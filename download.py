cf67ce903565b7bc1acaf19d1bc8275cfa97d424
Felipe Maziero <flpmaziero@gmail.com>
2024-02-29 01:39:19 -0300

    feat(dataframe): formating fields

--- download.py
+++ download.py
@@ -8,6 +8,7 @@ import base64
 import codecs
 import numpy as np
 from types import NoneType
+import csv
 
 
 def download_pdfs(file_path):
@@ -54,20 +55,24 @@ def download_series(file_path):
             if (series_reports == None):
                 print(f"WARN: escaping updates for={fiagro_code}")
                 continue
-            try:
-                attr = series_reports.get('data-options')
-                data = json.loads(attr)
-                df = None
-                for item in data['data']['items'][0]:
+            attr = series_reports.get('data-options')
+            data = json.loads(attr)
+            df = None
+            for page in data['data']['items']:
+                for item in page:
                     if (type(df) == NoneType):
                         df = pd.DataFrame(columns=item.keys())
                     new = pd.json_normalize(item)
                     df = pd.concat([df, new], ignore_index=True)
-                #df.to_csv('out.csv', index=False)
-                csv_filename = f'csvs/{fiagro_code}_series.csv'
-                print(f"Downloaded {csv_filename}...")
-            except:
-                print(f"Falha no download de {url}. Status={response.status_code}")
+                
+            # Formating
+            df['baseDate'] = pd.to_datetime(df['baseDate'], dayfirst=True)
+            df['paymentDate'] = pd.to_datetime(df['paymentDate'], dayfirst=True)
+            df['yield'] = df['yield'].apply(lambda x: float(x.replace(' %', '').replace(',', '.')))
+            df['revenue'] = df['revenue'].apply(lambda x: float(x.replace('R$ ', '').replace(',', '.')))
+            csv_filename = f'csvs/{fiagro_code}_series.csv'
+            df.to_csv(csv_filename, index=False, quoting=csv.QUOTE_ALL, sep=";")
+            print(f"Created {csv_filename}...")
 #               
 def main():
     now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
